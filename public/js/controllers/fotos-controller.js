4ba95022cc3ce4184f15a6cf15b40aacc9fca7fb
wanderson.silveira <wanderson.silveira23@gmail.com>
2024-01-31 09:57:29 -0300

    Initial template

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,8 @@
+angular.module('alurapic').controller('FotosController', function($scope){
+
+    $scope.foto = {
+        titulo: 'Onça',
+        url: 'https://www.infoescola.com/wp-content/uploads/2008/05/onca-pintada-591459416.jpg'
+    };
+
+});
